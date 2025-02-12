f23a8207016704909399e8cdb8fa997684451565
neblancsi <neblancsi95@gmail.com>
2025-02-12 15:41:37 +0100

    GitHub OAuth2 authorization

--- src/main/java/com/frankmoley/lil/adminweb/data/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.frankmoley.lil.adminweb.data.repository;
-
-import com.frankmoley.lil.adminweb.data.model.Customer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
