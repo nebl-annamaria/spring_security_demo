03198dae6a47f18cf7d786ac77b3fd4f30eb500e
neblancsi <neblancsi95@gmail.com>
2025-02-12 16:45:36 +0100

    WebFlux basic security

--- src/main/java/com/frankmoley/lil/adminweb/AdminWebApplication.java
+++ src/main/java/com/frankmoley/lil/adminweb/AdminWebApplication.java
@@ -2,16 +2,6 @@ package com.frankmoley.lil.adminweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class AdminWebApplication {
